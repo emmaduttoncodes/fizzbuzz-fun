ba706c2d4f6a7ff17732b9a941ff8aa6712b0b12
emmaduttoncodes <166871361+emmaduttoncodes@users.noreply.github.com>
2024-05-16 18:56:35 +0100

    Scaffolding
    Setting up project, unit tests and gitignore

---
+++ FizzBuzzGame/FizzBuzzGame.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace FizzBuzzGame.Tests;
+
+public class Tests
+{
+
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test()
+    {
+       
+    }
+}
---
+++ FizzBuzzGame/FizzBuzzGameTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace FizzBuzzGameTests;
+
+public class Tests
+{
+    Printer _printer;
+
+    [SetUp]
+    public void Setup()
+    {
+
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
