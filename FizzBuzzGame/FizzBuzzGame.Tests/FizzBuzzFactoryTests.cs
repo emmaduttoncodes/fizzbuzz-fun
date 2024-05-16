b7d5ff13c23f073444599ba1f603969141fd9f5d
emmaduttoncodes <166871361+emmaduttoncodes@users.noreply.github.com>
2024-05-16 21:01:50 +0100

    Add failing test: In FizzBuzz factory return input as string when not divisible by 15

---
+++ FizzBuzzGame/FizzBuzzGame.Tests/FizzBuzzFactoryTests.cs
@@ -0,0 +1,39 @@
+﻿namespace FizzBuzzGame.Tests;
+
+public class FizzBuzzFactoryTests
+{
+     private FizzBuzzFactory _fizzBuzzFactory;
+
+    [SetUp]
+    public void Setup()
+    {
+        _fizzBuzzFactory = new FizzBuzzFactory();
+    }
+
+    [Test]
+    public void FizzBuzzFactory_ReturnsFizzBuzzValueWhenInputDivisibleBy15()
+    {
+        //Arrange 
+        var input = 30;
+
+        //Act
+        var result = _fizzBuzzFactory.GetFizzBuzzValue(input);
+
+        //Assert
+        Assert.That(result, Is.EqualTo("FizzBuzz"));
+    }
+
+    [Test]
+    public void FizzBuzzFactory_ReturnsInputValueWhenInputNotDivisibleBy15()
+    {
+        //Arrange 
+        var input = 2;
+
+        //Act
+        var result = _fizzBuzzFactory.GetFizzBuzzValue(input);
+
+        //Assert
+        Assert.That(result, Is.EqualTo("2"));
+    }
+
+}
