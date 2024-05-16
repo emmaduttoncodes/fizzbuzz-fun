f011f934eb4b9f0738d6a8f36b4b45817b827921
emmaduttoncodes <166871361+emmaduttoncodes@users.noreply.github.com>
2024-05-16 20:02:32 +0100

    Add failing test: expect Fizz when divisable by 3

--- FizzBuzzGame/FizzBuzzGame.Tests/FizzBuzzCalculatorTests.cs
+++ FizzBuzzGame/FizzBuzzGame.Tests/FizzBuzzCalculatorTests.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualBasic;
+
 namespace FizzBuzzGame.Tests;
 
 public class FizzBuzzCalculatorTests
@@ -13,9 +15,21 @@ public class FizzBuzzCalculatorTests
     [Test]
     public void FizzBuzzCalculator_ReturnsListOfStrings()
     {
-        var result = _fizzBuzzCalculator.GetFizzBuzzList();
+        var result = _fizzBuzzCalculator.GetFizzBuzzList(1, 10);
 
         Assert.That(result, Is.Not.Null);
         Assert.That(result, Is.TypeOf<List<string>>());
+    }
+
+    [Test]
+    public void FizzBuzzCalculator_ReturnsFizzWhenInputIsDivisableBy3()
+    {
+        var startsAt = 3;
+        var increasesBy = 1;
+        var result = _fizzBuzzCalculator.GetFizzBuzzList(startsAt, increasesBy);
+
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result, Is.Not.Null);
+        Assert.That(result.SingleOrDefault, Is.EqualTo("Fizz"));
     }
 }
