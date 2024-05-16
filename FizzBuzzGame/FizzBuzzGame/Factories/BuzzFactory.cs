053da91cb39b65b3d5b77cfd411878570dc9598e
emmaduttoncodes <166871361+emmaduttoncodes@users.noreply.github.com>
2024-05-16 20:08:27 +0100

    Fix test: Implement factory to return Buzz when divisible by 5

---
+++ FizzBuzzGame/FizzBuzzGame/Factories/BuzzFactory.cs
@@ -0,0 +1,11 @@
+﻿namespace FizzBuzzGame;
+
+public class BuzzFactory : INumberFactory
+{
+    public int DivisableBy => 5;
+
+    public string GetFizzBuzzValue()
+    {
+        return "Buzz";
+    }
+}
