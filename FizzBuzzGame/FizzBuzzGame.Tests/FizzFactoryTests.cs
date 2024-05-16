fd73c167fda7d344f0d7235ab0aedf81523d9a13
emmaduttoncodes <166871361+emmaduttoncodes@users.noreply.github.com>
2024-05-16 20:58:51 +0100

    Add failing test: FizzFactory returns input as string if not divisible by 3

---
+++ FizzBuzzGame/FizzBuzzGame.Tests/FizzFactoryTests.cs
@@ -0,0 +1,38 @@
+﻿namespace FizzBuzzGame.Tests;
+
+public class FizzFactoryTests
+{
+    private FizzFactory _fizzFactory;
+
+    [SetUp]
+    public void Setup()
+    {
+        _fizzFactory = new FizzFactory();
+    }
+
+    [Test]
+    public void FizzFactory_ReturnsFizzValueWhenInputDivisibleBy3()
+    {
+        //Arrange 
+        var input = 3;
+
+        //Act
+        var result = _fizzFactory.GetFizzBuzzValue(input);
+
+        //Assert
+        Assert.That(result, Is.EqualTo("Fizz"));
+    }
+
+    [Test]
+    public void FizzFactory_ReturnsInputValueWhenInputNotDivisibleBy3()
+    {
+        //Arrange 
+        var input = 2;
+
+        //Act
+        var result = _fizzFactory.GetFizzBuzzValue(input);
+
+        //Assert
+        Assert.That(result, Is.EqualTo("2"));
+    }
+}
